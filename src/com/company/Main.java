857e5857e0554cda163539de3b15abe9292239fb
Billy Christopher <billy_christopher@hotmail.com>
2020-10-05 16:26:07 +0200

    Exercice Algo n°2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,39 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    static void tab(int[] nomTableau){
         int moyenne = 0;
+
+        for (int i = 0; i < nomTableau.length; i++){
+            System.out.println(nomTableau[i]);
+            moyenne = moyenne + nomTableau[i];
+        }
+        System.out.println("La moyenne des notes du tableau est : " + moyenne / nomTableau.length);
+    }
+
+    static int max(int[] nomTableau){
+        int max = 0;
+
+        for (int i = 0; i < nomTableau.length; i++){
+            if (nomTableau[i] > max){
+                max = nomTableau[i];
+            }
+        }
+        return max;
+    }
+
+    static boolean inferieur10(int[] nomTableau){
+        boolean inf10 = false;
+
+        for (int i = 0; i < nomTableau.length; i++){
+            if (nomTableau[i] < 10){
+                inf10 = true;
+            }
+        }
+        return inf10;
+    }
+
+    public static void main(String[] args) {
         int scores[] = new int[7];
         scores[0] = 3;
         scores[1] = 15;
@@ -13,10 +44,8 @@ public class Main {
         scores[5] = 4;
         scores[6] = 6;
 
-        for (int i = 0; i < scores.length; i++){
-            System.out.println(scores[i]);
-            moyenne = moyenne + scores[i];
-        }
-        System.out.println("La moyenne des notes du tableau est : " + moyenne / scores.length);
+        tab(scores);
+        System.out.println("Le plus grand nombre de ce tableau est : " + max(scores));
+        System.out.println(inferieur10(scores));
     }
 }
